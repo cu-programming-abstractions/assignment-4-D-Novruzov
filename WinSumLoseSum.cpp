5b446c84c7a83104554ea948cf21115037f01712
David Novruzov <novruzovdavcho@gmail.com>
2025-04-23 16:00:46 +0400

    commiting the functions

---
+++ WinSumLoseSum.cpp
@@ -0,0 +1,82 @@
+#include "WinSumLoseSum.h"
+#include <set>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+Optional<Set<int>> makeTarget(const Set<int>& elems, int target) {
+    if (target == 0) return {}; // return empty set
+    if (elems.isEmpty()) return Nothing;
+
+    int first = elems.first();
+    Set<int> rest = elems;
+    rest.remove(first);
+
+    // Include first
+    Optional<Set<int>> with = makeTarget(rest, target - first);
+    if (with != Nothing) {
+        with.value() += first;
+        return with;
+    }
+
+    // Excludefirst
+    Optional<Set<int>> without = makeTarget(rest, target);
+    if (without != Nothing) {
+        return without;
+    }
+
+    return Nothing;
+}
+
+
+
+/* * * * * Test Cases Below This Point * * * * */
+#include "GUI/SimpleTest.h"
+
+/* TODO: Add at least one custom test here, then delete this comment. */
+STUDENT_TEST("Works with negative numbers") {
+    /* Can make 0, but not others. */
+    EXPECT_EQUAL(makeTarget({-2, 3, 4, 6},  2), {-2, 4});
+}
+
+
+/* * * * * Provided Tests Below This Point * * * * */
+PROVIDED_TEST("Works for an empty set of numbers.") {
+    /* Can make 0, but not others. */
+    EXPECT_EQUAL(makeTarget({ },  0), {});
+    EXPECT_EQUAL(makeTarget({ },  1), Nothing);
+    EXPECT_EQUAL(makeTarget({ }, -1), Nothing);
+}
+
+PROVIDED_TEST("Works for a one-element (singleton) set.") {
+    /* Can make 0 and 137, but not others. */
+    EXPECT_EQUAL(makeTarget({ 137 }, 0),   {});
+    EXPECT_EQUAL(makeTarget({ 137 }, 137), {137});
+    EXPECT_EQUAL(makeTarget({ 137 }, 1),   Nothing);
+    EXPECT_EQUAL(makeTarget({ 137 }, -1),  Nothing);
+}
+
+PROVIDED_TEST("Works for a two-element (doubleton) set.") {
+    EXPECT_EQUAL(makeTarget({ 1, 2 }, -1), Nothing);
+    EXPECT_EQUAL(makeTarget({ 1, 2 }, 0), {});
+    EXPECT_EQUAL(makeTarget({ 1, 2 }, 1), {1});
+    EXPECT_EQUAL(makeTarget({ 1, 2 }, 2), {2});
+    EXPECT_EQUAL(makeTarget({ 1, 2 }, 3), {1, 2});
+    EXPECT_EQUAL(makeTarget({ 1, 2 }, 4), Nothing);
+}
+
+PROVIDED_TEST("Works for a three-element set.") {
+
+    /* Can make 0, 1, 3, 4, 5, 7, and 8, but not others. */
+    EXPECT_EQUAL(makeTarget({ 1, 3, 4 }, -1), Nothing);
+    EXPECT_EQUAL(makeTarget({ 1, 3, 4 },  0), {});
+    EXPECT_EQUAL(makeTarget({ 1, 3, 4 },  1), {1});
+    EXPECT_EQUAL(makeTarget({ 1, 3, 4 },  2), Nothing);
+    EXPECT_EQUAL(makeTarget({ 1, 3, 4 },  3), {3});
+    EXPECT_NOT_EQUAL(makeTarget({ 1, 3, 4 }, 4), Nothing);
+    EXPECT_EQUAL(makeTarget({ 1, 3, 4 },  5), {1, 4});
+    EXPECT_EQUAL(makeTarget({ 1, 3, 4 },  6), Nothing);
+    EXPECT_EQUAL(makeTarget({ 1, 3, 4 },  7), {3, 4});
+    EXPECT_EQUAL(makeTarget({ 1, 3, 4 },  8), {1, 3, 4});
+    EXPECT_EQUAL(makeTarget({ 1, 3, 4 },  9), Nothing);
+}
